94dd4bb090a0812322dd8c678a028050c386c988
Mounir Yacine <97355898+Mouniryacine@users.noreply.github.com>
2023-05-03 11:54:56 +0100

    finished Solution draw()

--- src/Models/Solution.java
+++ src/Models/Solution.java
@@ -7,6 +7,8 @@ package Models;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics;
+import static java.lang.Math.pow;
+import static java.lang.Math.sqrt;
 
 /**
  *
@@ -14,8 +16,7 @@ import java.awt.Graphics;
  */
 public class Solution extends Element {
     
-     
-    
+
     public Solution(){
         
     }
@@ -29,61 +30,139 @@ public class Solution extends Element {
     }
 
 
-//    protected void paintComponent(Graphics g) {
-//      
-//        
-//        g.setColor(Color.RED);
-//        g.setFont(new Font("Arial", Font.BOLD, 20));
-//        
-//        // Get the width and height of the text
-//        int textWidth = g.getFontMetrics().stringWidth(descreption);
-//        int textHeight = g.getFontMetrics().getHeight();
-//        
-//        // Calculate the position and size of the rectangle
-//        int rectX = (140 - textWidth) / 2 - 10; // Subtracting 10 to give some padding
-//        int rectY = (150 - textHeight) / 2 - 10;
-//        int rectWidth = textWidth + 20;
-//        int rectHeight = textHeight + 20;
-//        
-//        // Draw the rectangle
-//        g.drawRect(rectX, rectY, rectWidth, rectHeight);
-//        
-//        // Draw the text
-//        g.drawString(descreption, rectX + 10, rectY + textHeight);
-//    }
+    @Override
+    public boolean contains(int x, int y) {
+          // Calculate the coordinates of the corners of the object's bounding box
+    int x1 = this.X-width/2;
+    int y1 = this.Y-height/2;
+    int x2 = this.X + this.width/2+15;
+    int y2 = this.Y + this.height/2+10;
+    
+     System.out.println(this.X+":x   Solution    y:"+this.Y+"\n");
+
+    
+    System.out.print("Solution pressed");
+     
+    // Check if the specified point is inside the bounding box
+    if (x >= x1 && x <= x2 && y >= y1 && y <= y2) {
+       
+        return true;
+        
+    } else {
+        return false;
+    }
+} 
     
     public String getcontent() {
         String content = ID + "\n\n" + descreption;
         return content;
     }
      public void draw(Graphics g){
+
          draw(g,X,Y,getcontent());
      }
+     
+     public void calcule(Graphics g){
+         System.out.print("hello world");
+     }
     public  void draw(Graphics g,int X ,int Y,String content) {
-        
-         
+                double mou3amil = sqrt(2);
+                String id="";
                
-               g.setFont(new Font("Arial", Font.BOLD, 16));
-                 int textWidth=1 ;
-                 int lineWidth;
+                g.setFont(new Font("Arial", Font.BOLD, 16));
+                 int textWidth=0;
+                 int minTW= (int) pow(5,100);
                   int textHeight = g.getFontMetrics().getHeight();
-
-                g.setColor(Color.BLACK);
-                for (String line : content.split("\n")) {
+                  
+                int delimiterIndex = content.indexOf("\n\n");
+//  decouper le content en id et descreption
+                if (delimiterIndex != -1) {
+                     id = content.substring(0, delimiterIndex);
+                     content = content.substring(delimiterIndex + 2);
                     
-                g.drawString(line, X+ 10, Y+ textHeight);
+                }
+//  compter le width et le height de descreption entree par letulisateur
+                for (String line : content.split("\n")) {   
+            
                     textHeight += g.getFontMetrics().getHeight();
-                    lineWidth = g.getFontMetrics().stringWidth(line);
+                    int lineWidth = g.getFontMetrics().stringWidth(line);
                     if(textWidth<lineWidth){
                         textWidth=lineWidth;
+                       
+                    }
+                    if(minTW>lineWidth){
+                        minTW=lineWidth;
+                        
                     }
                 }
-                  g.drawRect(X, Y,textWidth+15,textHeight-10);
+                
+//  donner une valeur initial a finalTW --> le final width de descreption / le width de carre qu'om va ecrire in
+                int finalTW=textHeight+(textWidth-textHeight)/2;
+//  compter la valeur final de finalTW                
+                while(textHeight*3/2<finalTW && minTW*3/2<finalTW ){
+//                while(textHeight+20<finalTW ){
+                    System.out.println(textHeight+":H////while loop///W:"+finalTW);
+                   finalTW=textHeight+(finalTW-textHeight)/2;  
+                   textHeight += g.getFontMetrics().getHeight();
+                   System.out.println(textHeight+":H///////W:"+finalTW);
+                }
+                
+                 height=(int) (finalTW*mou3amil);
+                 width=height;
+                
+                    
+// buttonX,buttonY sont pour que la souris sera au centre de preview
+                int buttonX=(int) (X-finalTW*sqrt(2)/2);
+                int buttonY=(int) (Y-finalTW*sqrt(2)/2);
+//                this.X= buttonX;
+//                this.Y=buttonY;
+
+//word X une variable pour controller le                 
+                int wordX=0;
+                int wordY=0;
+                
+              
+                
+                textHeight = g.getFontMetrics().getHeight();
+                int  buttonYf=(int) (buttonY+((finalTW*sqrt(2)-finalTW)/2));
+                int  buttonXf=(int) (buttonX+((finalTW*sqrt(2)-finalTW)/2));
+                g.setColor(Color.BLUE);
+                g.drawRect(buttonXf, buttonYf,finalTW,finalTW);
+                g.setColor(Color.BLACK);
+                for (String line : content.split("\n")) {
+                    wordX= 0;
+                    wordY += g.getFontMetrics().getHeight();
+                    buttonYf+=g.getFontMetrics().getHeight();
+                        
+                for (String word : line.split(" ")) {
+                         word=word+" ";
+                    if(wordX+ g.getFontMetrics().stringWidth(word)<finalTW){
+                        g.drawString(word, buttonXf+wordX, buttonYf);
+                        wordX += g.getFontMetrics().stringWidth(word);
+                    }else{
+                        wordX=0;
+                        wordY += g.getFontMetrics().getHeight();
+                        buttonYf+=g.getFontMetrics().getHeight();
+                        g.drawString(word, buttonXf+wordX, buttonYf);
+                        wordX += g.getFontMetrics().stringWidth(word);
+                    }
+                    
+                }}
+                
+                
+                g.drawString(id, buttonX+ (int) (finalTW*mou3amil)/2- g.getFontMetrics().stringWidth(id)/2,buttonY+g.getFontMetrics().getHeight() );
+                
+                g.drawOval(buttonX, buttonY, (int) (finalTW*mou3amil),(int) (finalTW*mou3amil));
+                g.setColor(Color.RED);
+                g.drawRect(buttonX, buttonY,(int) (finalTW*mou3amil),(int) (finalTW*mou3amil));
+                
+               
                   
                  
-                    
-                    width=textWidth;
-                    height=textHeight;
+                
+                
+                  
+
     }
     
     
